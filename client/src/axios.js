130ad871e0e3d2a916347952993487092f1560c3
Justice <justicetobe449@gmail.com>
2025-09-09 22:58:11 +0100

    project

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL:"https://scanclean-backend.onrender.com/api",
+    baseURL:meta.env.VITE_API_URL || "http://localhost:6000/api",
     withCredentials:true,
 });
 
